bcfe6509b4a32fe61e1cd71b022fce666a5d6322
Luann <116429940+ANILuann@users.noreply.github.com>
2022-10-22 12:29:34 +0300

    Обновил программу
    Теперь вместо одного пишет другое

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
             string name;
-         Console.WriteLine("Введите Ваша имя": ");
+         Console.WriteLine("Введите имя": ");
          name =Console.ReadLine();
             Console.WriteLine("Приветствую Вас, " + name + "!");
         }
